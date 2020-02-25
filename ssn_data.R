e0f2d5bef8213f737c14d5846a1f4b51982380d9
howetowork <howetowork@gmail.com>
2020-02-25 06:53:26 -0500

    Uploaded new data

--- ssn_data.R
+++ ssn_data.R
@@ -19,8 +19,9 @@ df5 <- read.delim("data/20200209_to_20200212.txt", sep ="\t", stringsAsFactors =
 df6 <- read.delim("data/20200212_to_20200214.txt", sep ="\t", stringsAsFactors = FALSE)
 df7 <- read.delim("data/20200214_to_20200219.txt", sep ="\t", stringsAsFactors = FALSE)
 df8 <- read.delim("data/20200219_to_20200221.txt", sep ="\t", stringsAsFactors = FALSE)
-df9 <- read.delim("data/20200221_to_current.txt", sep ="\t", stringsAsFactors = FALSE)
-df <- rbind(df1, df2, df3, df4, df5, df6, df7, df8, df9)
+df9 <- read.delim("data/20200221_to_20200224.txt", sep ="\t", stringsAsFactors = FALSE)
+df10 <- read.delim("data/20200224_to_current.txt", sep ="\t", stringsAsFactors = FALSE)
+df <- rbind(df1, df2, df3, df4, df5, df6, df7, df8, df9, df10)
 
 # remove data outside target time ranges
 df$time_in_hours <- strptime(df$Time, format = "%H:%M:%S")
