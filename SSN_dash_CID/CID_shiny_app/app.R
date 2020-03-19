0984a1393c97bac660f03c8258a7e042c0d1caf4
howetowork <howetowork@gmail.com>
2020-03-19 18:12:32 -0400

    Added ability to copy link to current state of app

--- SSN_dash_CID/CID_shiny_app/app.R
+++ SSN_dash_CID/CID_shiny_app/app.R
@@ -1,10 +1,11 @@
 ### 717AA Outdoor Noise Monitoring
 ### Stephen Howe
 ### 19 March 2020
-### Version 3
+### Version 4
 
 ### Version Information ####
-# 20200318 V3: set fixed colors for various metrics
+# 20200319 V4: added URL parameters and bookmarking of current app state
+# 20200319 V3: set fixed colors for various metrics
 # 20200318 V2: interactive graphs, simplified logic to pull file; tz adjustment
 # 20200317 V1: initial version
 
@@ -26,7 +27,8 @@ set_logging(file=TRUE)
 # source AWS configs
 source("config.R")
 
-ui <- dashboardPage(skin = "yellow",
+ui <- function(request) {
+  dashboardPage(skin = "yellow",
                     
                     # set dashboard header ####
                     dashboardHeader(title = "Atlantic Avenue Noise"),
@@ -40,7 +42,7 @@ ui <- dashboardPage(skin = "yellow",
                                     choices = c("LAFmin", "LAFmax", "LAeq", "LAF01", "LAF10", "LAF50", "LAF90", "LAF99"),
                                     selected = c("LAFmin", "LAFmax", "LAeq"),
                                     multiple = TRUE),
-                        dateInput("date2", "Select Date (yyyy-mm-dd):"),
+                        dateInput("date", "Select Date (yyyy-mm-dd):"),
                         menuItem("Information", tabName = "information", icon = icon("info-circle"))
                       )
                     ),
@@ -62,35 +64,38 @@ ui <- dashboardPage(skin = "yellow",
                                   box(title = "Noise Readings for Selected Day",
                                       width = 12,
                                       plotlyOutput("plt_noise_readings")
-                                      )
                                   )
                                 ),
+                                bookmarkButton(label = "Copy Link to Graph")
+                        ),
                         
                         # information body ponel ####
                         tabItem(tabName = "information",
                                 fluidRow(
                                   box(title = "Hello World"))
-                                )
-                        ) # end of tab items
+                        )
+                      ) # end of tab items
                     ) # end of dashboard body
 ) # end of UI
+  
+}
 
 # Define server logic ----
 server <- function(input, output, session) {
   # initialize event logging
   set_logging_session()
-  
+    
   output$plt_noise_readings <- renderPlotly({
     # create file name based on selected date
     # FUTURE: substitue hardcoded device ID with user input
     selected_file <- paste("SSND_", 
                            "1453", 
                            "_",
-                           substring(input$date2, 1,4), 
+                           substring(input$date, 1,4), 
                            "_",
-                           substring(input$date2, 6,7), 
+                           substring(input$date, 6,7), 
                            "_",
-                           substring(input$date2, 9, 10),
+                           substring(input$date, 9, 10),
                            ".csv",
                            sep = "")
     
@@ -119,7 +124,7 @@ server <- function(input, output, session) {
               "LAFmin" = "#CC79A7")
     
     # plot
-    p1 <- ggplot(df_subset, aes(Time, value, color = factor(Measure))) +
+    p1 <- ggplot(df_subset, aes(Time, value, color = Measure)) +
       geom_line() +
       labs(title = "Outside Sounds Readings 717 Atlantic Avenue - 7th Floor",
            y = "Decibels") +
@@ -139,10 +144,11 @@ server <- function(input, output, session) {
 } #end of server functions
 
 ### Run the application ####
-shinyApp(ui = ui, server = server)
-                      
+shinyApp(ui = ui, server = server, enableBookmarking = "url")
+
 
 # sources ####
-# http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
+# color palettes http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
+# bookmarking app with current parameter selections: https://shiny.rstudio.com/articles/bookmarking-state.html
 
 
